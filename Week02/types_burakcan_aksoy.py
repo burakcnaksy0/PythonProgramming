94116a9b63db7f1ad619ab16c1cf2d0efbb0a524
burakcan aksoy <aksoyburak808@gmail.com>
2026-01-05 06:11:38 +0300

    type_burakcan_aksoy

---
+++ Week02/types_burakcan_aksoy.py
@@ -0,0 +1,11 @@
+# An integer with the name: my_int
+my_int = 42
+
+# A float with the name: my_float
+my_float = 3.14
+
+# A boolean with the name: my_bool
+my_bool = True
+
+# A complex with the name: my_complex
+my_complex = 1 + 2j
